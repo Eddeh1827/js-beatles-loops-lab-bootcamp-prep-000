fc03e247f19c8f4e2f11b85cc02b91e30f0206c8
Eddie Bickham <bickhame@gmail.com>
2017-10-08 09:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-function theBeatlesPlay(musicians,instruments) {
-  var phrases = [];
-  for (let i = 0; i < musicians.length; i++) {
-    phrases.push(musicians[i] + " plays " + instruments[i]);
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0;i < musicians.length; i++) {
+    array = 'musicians[i] + ' ' + instruments[i]'
+    return array;
+    
   }
-  return phrases;
-}
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "foo", "bar"];
-  var newFacts = [];
-  var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++;
-  }
-  return newFacts;
-}
-function iLoveTheBeatles(num) {
-  var phrases = [];
-  do {
-    phrases.push("I love the Beatles!");
-    num++
-  } while (num < 15);
-  return phrases;
 }
